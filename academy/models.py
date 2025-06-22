e8836cf7bd72b2396a20ad0a1afcec37b0a19d42
Seif Wael <wsaif204@gmail.com>
2025-06-22 15:55:00 +0200

    fix

--- academy/models.py
+++ academy/models.py
@@ -1,10 +1,7 @@
 # courses/models.py
 from django.db import models
-from cloudinary_storage.storage import MediaCloudinaryStorage # هذا الاستيراد قد لا يكون ضرورياً هنا لو لم تستخدم MediaCloudinaryStorage مباشرة في هذا الموديل
+from cloudinary_storage.storage import MediaCloudinaryStorage
 from cloudinary.models import CloudinaryField
-
-# قم بحذف السطر التالي تماماً، فهو غير مطلوب:
-# from .models import Course, CoursePDF # <--- احذف هذا السطر
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
@@ -13,7 +10,7 @@ class Course(models.Model):
     credit_hours = models.IntegerField()
     students = models.IntegerField(null=True, blank=True)
     course_description = models.TextField()
-    image_url = models.URLField(max_length=500, blank=True, null=True)
+    image_url = models.URLField(max_length=500, blank=True, null=True) # <--- أضف هذا الحقل
 
     def __str__(self):
         return self.name
@@ -21,10 +18,10 @@ class Course(models.Model):
 class CoursePDF(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='pdfs')
     pdf_file = CloudinaryField(
-        resource_type='raw',
+        resource_type='raw', 
         folder='damanhour/Staff/pdfs',
         blank=True, null=True
-    )
+        )
 
     def __str__(self):
         return f"{self.course.name} PDF"
